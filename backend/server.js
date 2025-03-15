1d68794faed2817e638b9ba7a92e2f9c244c367a
Adithyan G <techtoday038@gmail.com>
2025-03-15 14:28:08 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -19,5 +19,5 @@ app.use("/api/skills", skillRoutes);
 app.use("/api/profile", profileRoutes);
 app.use("/api", authRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
